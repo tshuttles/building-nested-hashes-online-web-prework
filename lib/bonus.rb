fda7228d41b6359659f47739d70c16698a3543da
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-15 05:49:14 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -25,5 +25,7 @@ def bonus
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   epic_tragedy
 end
